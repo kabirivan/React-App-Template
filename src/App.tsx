bca96d37bcf747bfb074c62b524b4438accdaa7b
IVAN <aguas.xavier2013a@gmail.com>
2022-03-26 23:43:17 -0500

    add login auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { useSelector, useDispatch } from 'src/redux/store';
 import { Toaster } from 'react-hot-toast';
 import { ThemeProvider } from '@mui/material/styles'
 import { createCustomTheme } from 'src/theme';
 import CssBaseline from '@mui/material/CssBaseline';
 import useSettings from 'src/hooks/useSettings';
-import FourKIcon from '@mui/icons-material/FourK';
-import { Button, Box } from '@mui/material';
-import { incrementAction, decrementAction, incrementByAmountAction } from 'src/redux/slices/counter'
+
 
 const App = () => {
   const { settings } = useSettings();
