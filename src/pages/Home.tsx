c685cb70bf5409ede9b1597d42d16dde422afa72
IVAN <aguas.xavier2013a@gmail.com>
2022-03-29 09:36:34 -0500

    Add encryptor in auth

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,31 @@
 import type { FC } from 'react';
 import { Helmet } from 'react-helmet-async';
 import HomeHero from 'src/components/HomeHero';
+import {
+  Box,
+  Container,
+} from '@mui/material';
 
 const Home: FC = () => {
-
   return (
     <>
       <Helmet>
         <title>JRTEC S.A.S.</title>
       </Helmet>
-      <div>
-        <HomeHero />
-      </div>
+      <Box
+        sx={{
+          backgroundColor: 'background.paper',
+          minHeight: '100%'
+        }}
+      >
+        <Box sx={{ py: 6 }}>
+          <Container maxWidth="lg">
+            <div>
+              <HomeHero />
+            </div>
+          </Container>
+        </Box>
+      </Box>
     </>
   );
 };
